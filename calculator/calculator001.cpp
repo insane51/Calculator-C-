3ab3a870011dff03f5dd6f63d525cffe11f4e297
insane51 <pkdriveonline@gmail.com>
2021-09-05 18:20:11 +0530

    simplecaculator class created

--- calculator/calculator001.cpp
+++ calculator/calculator001.cpp
@@ -1,6 +1,53 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class simplecalculator{
+    float a,b,res;
+    char operation; 
+    bool isoutput=true;   
+    public:
+        simplecalculator(){
+            a=0;b=0;res=0;
+        }
+        simplecalculator(float x,float y){
+            a=x;b=y;
+        }
+        void input(){
+            cout<<"Enter the values :"<<endl;
+            cin>>a>>b;
+            cout<<"Enter the operation :"<<endl;
+            cin>>operation;
+        }
+
+        void solution(){
+            switch(operation){
+                case '+':{
+                    res=a+b; break;
+                }
+                case '-':{
+                    res=a-b; break;
+                }
+                case '*':{
+                    res=a*b; break;
+                }
+                case '/':{
+                    if(b==0) cout<<"invalid b"<<endl,isoutput=false;
+                    else res=a/b;
+                    break;
+                }
+                default :{
+                    cout<<"Enter the values again with valid operation."<<endl;
+                    isoutput=false;
+                }
+            }
+
+        }
+
+        void print(){
+            if(isoutput) cout<<res<<endl;
+        }
+
+};
 
 int main(){
     cout<<"Welcome to the calculator"<<endl;

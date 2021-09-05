31f484ffab474a92783cda75f1f9bce454a60f77
insane51 <pkdriveonline@gmail.com>
2021-09-05 18:02:17 +0530

    main fxn prototype

--- calculator/calculator001.cpp
+++ calculator/calculator001.cpp
@@ -1,1 +1,30 @@
-  
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    cout<<"Welcome to the calculator"<<endl;
+    while(true){
+        cout<<"0.Exit"<<endl;
+        cout<<"1.Simple calculation"<<endl;
+        cout<<"2.Scintific calculation"<<endl;
+        int n; cin>>n;
+
+        switch(n){
+            case 0:{
+                cout<<"Thanks for using "<<endl;
+                return 0;
+            }
+
+            case 1:{
+
+            }
+
+            case 2:{
+
+            }
+
+        }
+    }
+
+}
